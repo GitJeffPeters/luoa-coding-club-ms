4d954825d815757e746700c7c99977e6eea0b4ab
Jeff Peters <jeff.peters@pragroup.com>
2024-01-18 17:08:25 -0500

    Jeff I'm adding circle program

---
+++ circle.py
@@ -0,0 +1,32 @@
+"""
+File: circle.py
+Project 7.1
+
+Draws a circle.
+
+1. The inputs are the coordinates of the center point and the radius.
+
+"""
+
+import math
+from turtle import Turtle
+
+def drawCircle(t, x, y, radius):
+    """Draws a circle with the given center point and radius."""
+    t.up()
+    t.goto(x + radius, y)
+    t.setheading(90)
+    t.down()
+    for count in range(120):
+        t.left(3)
+        t.forward(2.0 * math.pi * radius / 120.0)
+    
+
+def main():
+    x = 50
+    y = 75
+    radius = 100
+    drawCircle(Turtle(), x, y, radius)
+
+if __name__ == "__main__":
+    main()
